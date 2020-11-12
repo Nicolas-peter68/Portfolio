574d2e3c68eefc9553638eaea7f3177087aff1e4
Nicolas-peter68 <71389477+Nicolas-peter68@users.noreply.github.com>
2020-11-12 17:02:02 +0100

    première partie
    mise en place navbar, partie intro, début à propos de moi

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/dist/style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    <?php
+    require_once("header.html");
+    ?>
+    <section class="about">
+        <h1>A propos de moi</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus vero reiciendis praesentium cum ut, veritatis alias laboriosam ullam hic, commodi eaque at nam nulla earum officiis doloribus cupiditate dolorum sit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae, omnis veritatis ratione earum maiores inventore quos, iste, vero perspiciatis perferendis commodi accusantium doloremque assumenda deleniti ex? Blanditiis rerum repellendus error. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad fugit provident perferendis tempore assumenda qui illo hic illum perspiciatis eaque, cumque non accusantium explicabo doloremque, sit reiciendis fuga nostrum? Voluptatem!</p>
+    </section>
+</body>
+</html>
