4a7de7e29610399361b9d143dca61da413057990
Nicolas-peter68 <71389477+Nicolas-peter68@users.noreply.github.com>
2020-11-13 17:00:05 +0100

    amélioration design et responsive
    ajout section projet, mise en place colonnes bootstrap

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/dist/style.css">
     <title>Document</title>
@@ -17,8 +16,36 @@
     require_once("header.html");
     ?>
     <section class="about">
-        <h1>A propos de moi</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus vero reiciendis praesentium cum ut, veritatis alias laboriosam ullam hic, commodi eaque at nam nulla earum officiis doloribus cupiditate dolorum sit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae, omnis veritatis ratione earum maiores inventore quos, iste, vero perspiciatis perferendis commodi accusantium doloremque assumenda deleniti ex? Blanditiis rerum repellendus error. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad fugit provident perferendis tempore assumenda qui illo hic illum perspiciatis eaque, cumque non accusantium explicabo doloremque, sit reiciendis fuga nostrum? Voluptatem!</p>
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-md-5">
+                    <h2>A propos de moi</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus vero reiciendis praesentium cum ut, veritatis alias laboriosam ullam hic, commodi eaque at nam nulla earum officiis doloribus cupiditate dolorum sit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae, omnis veritatis ratione earum maiores inventore quos, iste, vero perspiciatis perferendis commodi accusantium doloremque assumenda deleniti ex? Blanditiis rerum repellendus error. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad fugit provident perferendis tempore assumenda qui illo hic illum perspiciatis eaque, cumque non accusantium explicabo doloremque, sit reiciendis fuga nostrum? Voluptatem!</p>
+                    <button type="button" class="btn btn-primary">Télécharger mon CV</button>
+                </div>
+                <div class="col-md-7">
+                    <div class="photo">photo</div>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    <section class="projets">
+        <div class="container">
+            <h2>Mes projets</h2>
+            <div class="row">
+                <p class="col-md-3 col-12">Projet1</p>
+                <p class="col-md-3 col-12">Projet1</p>
+                <p class="col-md-3 col-12">Projet1</p>
+                <p class="col-md-3 col-12">Projet1</p>
+                <p class="col-md-3 col-12">Projet1</p>
+                <p class="col-md-3 col-12">Projet1</p>
+                <p class="col-md-3 col-12">Projet1</p>
+                <p class="col-md-3 col-12">Projet1</p>
+            </div>
+        </div>
     </section>
 </body>
+<!--https://preview.colorlib.com/theme/orbit/#contact-section-->
+
 </html>
